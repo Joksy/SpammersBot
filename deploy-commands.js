f48faa59b1340f39d59f6d52360a5ed0d87c4064
Joksy <107338898+Joksy@users.noreply.github.com>
2022-09-09 09:46:25 +0200

    commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,11 +6,12 @@ const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('spam').setDescription('Spams!'),
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
